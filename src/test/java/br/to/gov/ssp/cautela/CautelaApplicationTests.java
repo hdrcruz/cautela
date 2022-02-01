0cd6ad006a0dfd3ffff2a13e3bbc8a9dd135a68e
Helder Cruz <hdr.cruz@gmail.com>
2022-02-01 10:15:49 -0300

    incluir código Spring Initializr

---
+++ src/test/java/br/to/gov/ssp/cautela/CautelaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.to.gov.ssp.cautela;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CautelaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
