29dda3aac2ac16bc09eb337d3f33f2ddde763049
Helder Cruz <hdr.cruz@gmail.com>
2022-02-01 10:28:55 -0300

    criar entidades

---
+++ src/main/java/br/to/gov/ssp/cautela/entity/Servidor.java
@@ -0,0 +1,43 @@
+package br.to.gov.ssp.cautela.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Servidor {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String sobrenome;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+    private List<Arma> phones = new ArrayList<>();
+
+}
