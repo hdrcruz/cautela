439fc2b6e564773d638f4f000f6bddb50193c03a
Helder Cruz <hdr.cruz@gmail.com>
2022-02-01 10:43:10 -0300

    criar repository

---
+++ src/main/java/br/to/gov/ssp/cautela/repository/ServidorRepository.java
@@ -0,0 +1,9 @@
+package br.to.gov.ssp.cautela.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.to.gov.ssp.cautela.entity.Servidor;
+
+public interface ServidorRepository extends JpaRepository<Servidor, Long>{
+
+}
